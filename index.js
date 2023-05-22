385f0e3af3834a5e36adc79320ee457505e8fe6f
Addison Philips <addyphilips66@gmail.com>
2023-05-22 20:16:48 +0100

    'complete'

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config('.env');
-const cors = require('cors');
-const express = require('express');
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 const { PORT = 3000 } = process.env;
 // TODO - require express-openid-connect and destructure auth from it
 const { auth } = require("express-openid-connect");
@@ -28,7 +29,7 @@ const {
 } = process.env;
 
 const config = {
-  authRequired: true, // this is different from the documentation
+  authRequired: false, // this is different from the documentation
   auth0Logout: true,
   secret: AUTH0_SECRET,
   baseURL: AUTH0_AUDIENCE,
@@ -38,6 +39,17 @@ const config = {
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
+
+app.use(async (req, res, next) => {
+  const [user] = await User.findOrCreate({
+    where: {
+      name: req.oidc.user.name,
+      email: req.oidc.user.email,
+    },
+  });
+  console.log(user);
+  next();
+});
 
 // req.isAuthenticated is provided from the auth router
 app.get("/", (req, res) => {
@@ -54,11 +66,57 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
+app.use((req, res, next) => {
+  const authHeader = req.header("Authorization");
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.user = decoded;
+    } catch (error) {
+      console.error(error);
+      res.status(401).send({ error: "Invalid token" });
+      return;
+    }
+  }
+  next();
+});
 
-app.get('/cupcakes', async (req, res, next) => {
+app.get("/cupcakes", async (req, res, next) => {
   try {
-    const cupcakes = await Cupcake.findAll();
-    res.send(cupcakes);
+    if (!req.user) {
+      res.status(401).send({ error: "Unauthorized" });
+      return;
+    }
+
+    const { title, flavor, stars } = req.body;
+    const createdCupcake = await Cupcake.create({
+      title,
+      flavor,
+      stars,
+      userId: req.user.id,
+    });
+
+    res.send(createdCupcake);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+app.get("/me", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { name: req.oidc.user.name },
+      raw: true,
+    });
+
+    if (user) {
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1w" });
+      res.send({ user, token });
+    } else {
+      res.status(404).send({ error: "User not found" });
+    }
   } catch (error) {
     console.error(error);
     next(error);
