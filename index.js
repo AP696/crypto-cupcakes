c98160759f9760160047a5e52daa06ef82eab8be
Addison Philips <addyphilips66@gmail.com>
2023-05-22 20:35:20 +0100

    'fixed'

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/cupcakes", async (req, res, next) => {
+app.post("/cupcakes", async (req, res, next) => {
   try {
     if (!req.user) {
       res.status(401).send({ error: "Unauthorized" });
